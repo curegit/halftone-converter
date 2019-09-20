a98d32d6ccfae09df851635e6d4ca8f9e788ca1e
Cure Git <37978051+curegit@users.noreply.github.com>
2019-09-20 20:29:06 +0900

    Update radius function

--- halftoning.py
+++ halftoning.py
@@ -1,39 +1,51 @@
 import argparse as arg
-import htfunctions as ht
+from math import sqrt, sin, acos, pi
 
-def make_fill_rate(pitch):
-    def func(radius):
-        if radius < 0:
-            return 0.0
-        elif radius < pitch / 2:
-            return radius * radius * pi / (pitch * pitch)
-        elif radius < sqrt(2.0) * pitch / 2:
-            return (radius * radius * pi - 4 * radius * radius * acos(pitch / (2 * radius)) + 2 * pitch * radius * sin(acos(pitch / (2 * radius)))) / (pitch * pitch)
-        else:
-            return 1.0
-    return func
+def make_occupancy(pitch):
+	def occupancy(radius):
+		if radius < 0:
+			return 0.0
+		elif radius < pitch / 2:
+			return pi * radius ** 2 / pitch ** 2
+		elif radius < sqrt(2) / 2 * pitch:
+			theta = acos(pitch / (2 * radius))
+			return (radius / pitch) ** 2 * (pi - 4 * theta) + 2 * radius * sin(theta) / pitch
+		else:
+			return 1.0
+	return occupancy
 
-def make(pitch, s=256):
-    table = []
-    f = make_fill_rate(pitch)
-    r = 0.0
-    d = 0.0001
-    while True:
-        #rn = r + d
-        if f(r) >= len(table) / (s-1):
-            table.append(r)
-        r += d
-        if len(table) == s:
-            break
-    def h(v):
-        if v < 0:
-            return 0.0
-        if v >= s:
-            return math.sqrt(2.0) * pitch / 2.0
-        else:
-            return table[v]
-    return h
+def radius_table(pitch, depth):
+	color = 0
+	occupancy = 0.0
+	f = make_occupancy(pitch)
+	while occupancy <= pi / 4:
+		yield pitch * sqrt(occupancy / pi)
+		color += 1
+		occupancy = color / (depth - 1)
+	r = 2 / pitch
+	while color < depth - 1:
+		while f(r) < occupancy:
+			r += 0.00000001
+		#yield r
+		error = f(r) - occupancy
+		yield error
+		print(f"error = {error}\n")
+		color += 1
+		occupancy = color / (depth - 1)
+	yield sqrt(2) / 2 * pitch
 
+def make_radius(pitch, depth):
+	table = list(radius_table(pitch, depth))
+	def radius(occupancy):
+		if occupancy < 0:
+			return 0.0
+		elif occupancy >= depth:
+			return sqrt(2) / 2 * pitch
+		else:
+			return table[occupancy]
+	return radius
+
+'''
 # コマンドライン引数をパース
 parser = arg.ArgumentParser(allow_abbrev=False, description="Halftoning ")
 parser.add_argument("images", metavar="FILE", nargs="+", help="")
@@ -55,17 +67,18 @@ print(args.directory)
 # 引数のバリデーションと展開
 directory = args.directory if args.direcotry != None else ""
 rgb_profile = args.
-cmyk_profile = 
+cmyk_profile =
 force = args.force
 
 radius
 pitch
-c = 
+c =
 m =
-y = 
+y =
 k =
 
 # 出力ディレクトリの確認
 
 #
 for (i, image) in enumerate(images):
+'''
