55e6009506230a270a675241e4dc4e0de8a3f4b4
Cure Git <37978051+curegit@users.noreply.github.com>
2019-09-27 13:20:26 +0900

    Insert comments

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@ from math import floor, ceil, sqrt, sin, cos, acos, pi
 from PIL import Image, ImageFilter, ImageCms
 from cairo import ImageSurface, Context, Antialias, Filter, FORMAT_ARGB32, OPERATOR_SOURCE
 
+# ドット半径から占有率を返す関数を返す
 def make_occupancy(pitch):
 	def occupancy(radius):
 		if radius < 0:
@@ -17,6 +18,7 @@ def make_occupancy(pitch):
 			return 1.0
 	return occupancy
 
+# 2分法による求根
 def bisection(f, x1, x2, eps):
 	while True:
 		x = (x1 + x2) / 2
@@ -28,6 +30,7 @@ def bisection(f, x1, x2, eps):
 		else:
 			x2 = x
 
+# 占有率からドット半径への変換テーブルをつくる
 def radius_table(pitch, depth):
 	color = 0
 	occupancy = 0.0
@@ -46,6 +49,7 @@ def radius_table(pitch, depth):
 		occupancy = color / (depth - 1)
 	yield rmax
 
+# 占有率からドット半径を返す関数を返す
 def make_radius(pitch, depth):
 	table = list(radius_table(pitch, depth))
 	def radius(occupancy):
@@ -57,6 +61,7 @@ def make_radius(pitch, depth):
 			return table[occupancy]
 	return radius
 
+# ピクセル空間から網点空間への変換と逆変換をする関数を返す
 def make_transforms(pitch, angle, origin=(0.0, 0.0)):
 	theta = angle / 180 * pi
 	def transform(x, y):
@@ -73,6 +78,7 @@ def make_transforms(pitch, angle, origin=(0.0, 0.0)):
 		return (x, y)
 	return (transform, inverse_transform)
 
+# シングルバンドの画像から網点の位置と階調のイテレータを返す
 def halftone_dots(image, pitch, angle, depth, blur):
 	center = (image.width / 2, image.height / 2)
 	transform, inverse_transform = make_transforms(pitch, angle, center)
@@ -90,6 +96,7 @@ def halftone_dots(image, pitch, angle, depth, blur):
 		color = blurred.getpixel((min(max(x, 0), image.width-1), min(max(y, 0), image.height-1)))
 		yield (x, y, color)
 
+# シングルバンドの画像を網点化した画像を返す
 def halftone_image(image, pitch, angle=45, scale=1.0, blur=None, keep_flag=False):
 	width = round(image.width * scale)
 	height = round(image.height * scale)
@@ -115,6 +122,7 @@ def halftone_image(image, pitch, angle=45, scale=1.0, blur=None, keep_flag=False
 		context.fill()
 	return Image.frombuffer("RGBA", (width, height), surface.get_data(), "raw", "RGBA", 0, 1).getchannel("G")
 
+# CMYKの画像を網点化した画像を返す
 def halftone_cmyk_image(image, pitch, angles=(15, 75, 30, 45), scale=1.0, blur=None, keep_flags=(False, False, False, False)):
 	c, m, y, k = image.split()
 	cyan = halftone_image(c, pitch, angles[0], scale, blur, keep_flags[0])
@@ -123,6 +131,7 @@ def halftone_cmyk_image(image, pitch, angles=(15, 75, 30, 45), scale=1.0, blur=N
 	key = halftone_image(k, pitch, angles[3], scale, blur, keep_flags[3])
 	return Image.merge("CMYK", [cyan, magenta, yellow, key])
 
+# プロファイル変換のラッパー関数を返す
 def make_profile_conversion(profiles, intent, mode):
 	in_profile, out_profile = profiles
 	def profile_conversion(image):
