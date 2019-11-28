1cef8bfd4fc2e83f9ad995e00abcea527a400e84
Cure Git <37978051+curegit@users.noreply.github.com>
2019-11-28 15:01:49 +0900

    Split files

--- halftoning.py
+++ halftoning.py
@@ -1,53 +1,72 @@
+from os.path import basename
 from argparse import ArgumentParser
 from PIL import Image, ImageCms
-from utilities import mkdirp, filepath, filerelpath, altfilepath
-from functions import halftone_grayscale_image, halftone_cmyk_image, make_profile_transform
+from modules.util import mkdirp, filepath, filerelpath, altfilepath
+from modules.color import make_profile_transform, make_fake_transforms
+from modules.core import halftone_grayscale_image, halftone_cmyk_image, halftone_rgb_image
 
 # コマンドライン引数をパース
 parser = ArgumentParser(allow_abbrev=False, description="Halftoning ")
 parser.add_argument("images", metavar="FILE", nargs="+", help="")
+
+
 parser.add_argument("-d", "--directory", help="")
 parser.add_argument("-p", "--prefix", help="")
 parser.add_argument("-s", "--suffix", help="")
 parser.add_argument("-f", "--force", action="store_true", help="")
 parser.add_argument("-e", "--enumerate", action="store_true", help="")
+
+
 parser.add_argument("-c", "--cmyk", action="store_true", help="")
 parser.add_argument("-b", "--bands", action="store_true", help="")
 parser.add_argument("-x", "--pitch", action="store_true", help="")
+
+gh = parser.add_argument_group("cmyk")
+gh.add_argument("-agn")
+blur_group = gh.add_mutually_exclusive_group()
+blur_group.add_argument("--no-blur", action="store_true", help="aa")
+blur_group.add_argument("--box-blur", action="store_true", help="bb")
+blur_group.add_argument("--gaussian-blur", action="store_true", help="cc")
+
 parser.add_argument("-a", "--angles", metavar="C,M,Y,K", help="")
+
 parser.add_argument("-ip", "--input-profile", help="")
 parser.add_argument("-cp", "--cmyk-profile", help="")
 parser.add_argument("-op", "--output-profile", help="")
 parser.add_argument("-ig", "--ignore-embedded-profile", action="store_true", help="")
-parser.add_argument("-kc", "--keep-cyan", action="store_true", help="")
-parser.add_argument("-km", "--keep-magenta", action="store_true", help="")
-parser.add_argument("-ky", "--keep-yellow", action="store_true", help="")
-parser.add_argument("-kk", "--keep-key", action="store_true", help="")
-parser.add_argument("-kb", "--keep-black", action="store_true", help="")
-parser.add_argument("-nb", "--no-blur", action="store_true", help="")
-parser.add_argument("-bb", "--box-blur", action="store_true", help="")
-parser.add_argument("-gb", "--gaussian-blur", action="store_true", help="")
+
+parser.add_argument("--keep-cyan", action="store_true", help="")
+parser.add_argument("--keep-magenta", action="store_true", help="")
+parser.add_argument("--keep-yellow", action="store_true", help="")
+parser.add_argument("--keep-key", action="store_true", help="")
+parser.add_argument("--keep-black", action="store_true", help="")
+
+
+
+
 args = parser.parse_args()
 
-# debug
-print(vars(args))
-print(args.directory)
 
 # 引数のバリデーションと展開
-#directory = args.directory if args.direcotry != None else ""
-#rgb_profile = args
-#cmyk_profile =
-#force = args.force
+directory = args.directory
+prefix = basename(args.prefix)
+suffix = basename(args.suffix)
+force = args.force
+enum = args.enumerate
 
 pitch = 2
 keep_flags = (args.keep_cyan, args.keep_magenta, args.keep_yellow, args.keep_key or args.keep_black)
+
+
 
 if len([f for f in [args.no_blur, args.box_blur, args.gaussian_blur] if f]) > 1:
 	print("")
 	exit()
 blur = None if args.no_blur else "box" if args.box_blur else "gaussian"
 
-# 出力ディレクトリの確認
+
+
+
 
 out_cmyk = False
 
@@ -61,10 +80,13 @@ out_profile = in_profile
 
 ignore_profile = args.ignore_embedded_profile
 
-srgb =
+#srgb =
 
 pt_cmyk = make_profile_transform((in_profile, cmyk_profile), ("RGB", "CMYK"), cmyk_initent, not ignore_profile)
 pt_rgb = make_profile_transform((cmyk_profile, out_profile), ("CMYK", "RGB"), rgb_intent, False)
+
+# 出力ディレクトリの確認
+mkdirp(directory)
 
 
 # メインループ
---
+++ modules/color.py
@@ -0,0 +1,76 @@
+from io import BytesIO
+from PIL import Image, ImageCms
+from numpy import frompyfunc, frombuffer, uint8, float64, rint
+
+# プロファイル変換のラッパー関数を返す
+def make_profile_transform(profiles, modes, intent, prefer_embedded=True):
+	transform = ImageCms.buildTransform(*profiles, *modes, intent)
+	def profile_conversion(image):
+		maybe_icc = image.info.get("icc_profile")
+		if not prefer_embedded or maybe_icc == None:
+			return ImageCms.applyTransform(image, transform)
+		em_profile = ImageCms.ImageCmsProfile(BytesIO(maybe_icc))
+		return ImageCms.profileToProfile(image, em_profile, profiles[1], renderingIntent=intent, outputMode=modes[1])
+	return profile_conversion
+
+# sRGBのガンマ変換
+def gamma_forward(u):
+	if u <= 0.0031308:
+		return 12.92 * u
+	else:
+		return 1.055 * u ** (1 / 2.4) - 0.055
+
+# sRGBの逆ガンマ変換
+def gamma_reverse(u):
+	if u <= 0.04045:
+		return u / 12.92
+	else:
+		return ((u + 0.055) / 1.055) ** 2.4
+
+# 近似によるsRGBとCMYKの色の変換関数を返す
+def make_fake_conversions(k_threshold, gamma_correction):
+	def rgb_2_cmyk(r, g, b):
+		if gamma_correction:
+			r, g, b = gamma_reverse(r), gamma_reverse(g), gamma_reverse(b)
+		k = max(0, min(1, (min(1 - r, 1 - g, 1 - b) - k_threshold) / (1 - k_threshold)))
+		c = 0.0 if abs(1 - k) <= float_info.epsilon * 4 else max(0, min(1, (1 - r - k) / (1 - k)))
+		m = 0.0 if abs(1 - k) <= float_info.epsilon * 4 else max(0, min(1, (1 - g - k) / (1 - k)))
+		y = 0.0 if abs(1 - k) <= float_info.epsilon * 4 else max(0, min(1, (1 - b - k) / (1 - k)))
+		return c, m, y, k
+	def cmyk_2_rgb(c, m, y, k):
+		r = min(1, 1 - min(1, c * (1 - k) + k))
+		g = min(1, 1 - min(1, m * (1 - k) + k))
+		b = min(1, 1 - min(1, y * (1 - k) + k))
+		if gamma_correction:
+			r, g, b = gamma_forward(r), gamma_forward(g), gamma_forward(b)
+		return r, g, b
+	return rgb_2_cmyk, cmyk_2_rgb
+
+# 近似によるsRGBとCMYKの画像の変換関数を返す
+def make_fake_transforms(k_threshold=0.5, gamma_correction=True):
+	rgb2cmyk, cmyk2rgb = make_fake_conversions(k_threshold, gamma_correction)
+	rgb2cmyk_univ = frompyfunc(rgb2cmyk, 3, 4)
+	cmyk2rgb_univ = frompyfunc(cmyk2rgb, 4, 3)
+	def rgb_2_cmyk(image):
+		r, g, b = image.split()
+		r_array = frombuffer(r.tobytes(), dtype=uint8) / 255
+		g_array = frombuffer(g.tobytes(), dtype=uint8) / 255
+		b_array = frombuffer(b.tobytes(), dtype=uint8) / 255
+		cmyk_array = rgb2cmyk_univ(r_array, g_array, b_array)
+		c = Image.frombuffer("L", (image.width, image.height), rint(cmyk_array[0].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		m = Image.frombuffer("L", (image.width, image.height), rint(cmyk_array[1].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		y = Image.frombuffer("L", (image.width, image.height), rint(cmyk_array[2].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		k = Image.frombuffer("L", (image.width, image.height), rint(cmyk_array[3].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		return Image.merge("CMYK", [c, m, y, k])
+	def cmyk_2_rgb(image):
+		c, m, y, k = image.split()
+		c_array = frombuffer(c.tobytes(), dtype=uint8) / 255
+		m_array = frombuffer(m.tobytes(), dtype=uint8) / 255
+		y_array = frombuffer(y.tobytes(), dtype=uint8) / 255
+		k_array = frombuffer(k.tobytes(), dtype=uint8) / 255
+		rgb_array = cmyk2rgb_univ(c_array, m_array, y_array, k_array)
+		r = Image.frombuffer("L", (image.width, image.height), rint(rgb_array[0].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		g = Image.frombuffer("L", (image.width, image.height), rint(rgb_array[1].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		b = Image.frombuffer("L", (image.width, image.height), rint(rgb_array[2].astype(float64) * 255).astype(uint8), "raw", "L", 0, 1)
+		return Image.merge("RGB", [r, g, b])
+	return rgb_2_cmyk, cmyk_2_rgb
