406655f65747243382aa25550eaf1d289cb9728b
curegit <37978051+curegit@users.noreply.github.com>
2020-02-27 17:46:50 +0900

    Update

--- halftoning.py
+++ halftoning.py
@@ -10,22 +10,30 @@ parser = ArgumentParser(allow_abbrev=False, description="Halftoning")
 parser.add_argument("images", metavar="FILE", nargs="+", help="")
 parser.add_argument("-d", "--directory", metavar="DIR", help="")
 parser.add_argument("-f", "--force", action="store_true", help="")
-parser.add_argument("-p", "--prefix", help="")
-parser.add_argument("-s", "--suffix", help="")
+parser.add_argument("-P", "--prefix", help="")
+parser.add_argument("-S", "--suffix", help="")
 parser.add_argument("-e", "--enumerate", metavar="START", type=int, nargs="?", const=1, help="")
-parser.add_argument("--tiff", action="store_true", help="")
+
+
+
 parser.add_argument("--gray", action="store_true", help="")
 parser.add_argument("--cmyk", action="store_true", help="")
 parser.add_argument("--rgb", action="store_true", help="")
-parser.add_argument("-P", "--pitch", metavar="PX", type=positive, help="")
+
+parser.add_argument("--", action="store_true", help="")
+parser.add_argument("--tiff", action="store_true", help="")
+
+parser.add_argument("-p", "--pitch", metavar="PX", type=positive, help="")
+parser.add_argument("-s", "--scale", metavar="PX", type=positive, help="")
 parser.add_argument("-b", "--blur", choices=["none", "box", "gaussian"], default="gaussian", help="")
 parser.add_argument("--channels", action="store_true", help="")
-
+parser.add_argument("--split", action="store_true", help="")
 parser.add_argument("-I", "--input-profile", help="")
 parser.add_argument("-C", "--cmyk-profile", help="")
 parser.add_argument("-R", "--rgb-profile", help="")
 #parser.add_argument("-O", "--output-profile", help="")
 parser.add_argument("-G", "--ignore-embedded-profile", action="store_true", help="")
+parser.add_argument("-F", "--fake", action="store_true", help="")
 cmyk_initent = 1
 rgb_intent = 3
 
@@ -45,12 +53,8 @@ rgb_group.add_argument("--keep-blue", action="store_true", help="")
 args = parser.parse_args()
 
 
-pitch = max(0.1, args.pitch)
-
-
-cmyk_angles = ()
-cmyk_keep_flags = (args.keep_cyan, args.keep_magenta, args.keep_yellow, args.keep_key or args.keep_black)
-rgb_keep_flags = ()
+cmyk_keep_flags = (args.keep_cyan, args.keep_magenta, args.keep_yellow, args.keep_key)
+rgb_keep_flags = (args.keep_red, args.keep_green, args.keep_blue)
 
 in_profile = ImageCms.createProfile("sRGB")
 cmyk_profile = "profile/JapanColor2011Coated.icc"
@@ -70,20 +74,58 @@ for i, f in enumerate(args.images, 1):
 	try:
 		img = Image.open(f)
 		if img.mode == "L":
-			pass
 		elif img.mode == "CMYK":
-			pass
-		elif img.mode == "RGB":
+			if args.mode == "gray":
+
+			elif args.mode == "cmyk":
+
+			elif args.mode == "rgb":
+
+		elif img.mode == "RGB" or img.mode == "RGBA":
+			if img.mode == "RGBA":
+				img = img.convert("RGB")
+			if args.mode == "gray":
+
+			elif args.mode == "cmyk":
+
+			elif args.mode == "rgb":
+		else:
+			raise ()
+
 
 			cmyk = pt_cmyk(img)
 
-			halftone = halftone_cmyk_image(cmyk, pitch, blur=blur, keep_flags=keep_flags)
+			halftone = halftone_cmyk_image(cmyk, args.pitch, angles=args.cmyk_angles, scale=args.scale, blur=args.blur, keep_flags=cmyk_keep_flags)
 			if out_cmyk:
-				halftone.save(f"{i}.png")
+
 			else:
 				pt_rgb(halftone).save(f"{i}.png")
 		else:
 			pass
+
+		if tg.mode == "L":
+
+		elif tg.mode == "CMYK":
+
+		elif tg.mode == "RGB":
+
+
+		if halftone.mode == "L":
+
+		elif halftone.mode == "CMYK":
+
+		elif halftone.mode == "RGB":
+
+
+		if complete.mode == "L":
+
+		elif complete.mode == "CMYK":
+			complete.save(f"{}.tiff")
+		elif complete.mode == "RGB":
+
+
+
+
 		print(f"{i} / {n} Done")
 	except Exception as e:
 		eprint(f"{i} / {n} Error")
