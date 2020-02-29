eb89f99a74391c7555bb9a348b95cc71817ab656
curegit <37978051+curegit@users.noreply.github.com>
2020-02-29 17:54:23 +0900

    Update

--- halftoning.py
+++ halftoning.py
@@ -8,11 +8,11 @@ from modules.core import halftone_grayscale_image, halftone_cmyk_image, halftone
 # コマンドライン引数をパース
 parser = ArgumentParser(allow_abbrev=False, description="Halftoning")
 parser.add_argument("images", metavar="FILE", nargs="+", help="")
-parser.add_argument("-d", "--directory", metavar="DIR", help="")
+parser.add_argument("-d", "--directory", metavar="DIR", default=".", help="")
 parser.add_argument("-f", "--force", action="store_true", help="")
-parser.add_argument("-P", "--prefix", help="")
-parser.add_argument("-S", "--suffix", help="")
-parser.add_argument("-e", "--enumerate", metavar="START", type=int, nargs="?", const=1, help="")
+parser.add_argument("-P", "--prefix", default="", help="")
+parser.add_argument("-S", "--suffix", default="halftone", help="")
+parser.add_argument("-E", "--enumerate", metavar="START", type=int, nargs="?", const=1, help="")
 
 
 
@@ -65,30 +65,43 @@ ignore_profile = args.ignore_embedded_profile
 pt_cmyk = make_profile_transform((in_profile, cmyk_profile), ("RGB", "CMYK"), cmyk_initent, not ignore_profile)
 pt_rgb = make_profile_transform((cmyk_profile, out_profile), ("CMYK", "RGB"), rgb_intent, False)
 
-# 出力ディレクトリの確認
+# 出力ディレクトリを作る
 mkdirp(args.directory)
 
+# Glob
+if args.glob:
+	input_images = []
+	for i in args.images:
+		input_images += glob(i, recursive=True)
+else:
+	input_images = args.images
+
 # メインループ
-n = len(args.images)
-for i, f in enumerate(args.images, 1):
+n = len(input_images)
+for i, f in enumerate(input_images, 1):
 	try:
+		#
 		img = Image.open(f)
 		if img.mode == "L":
+			target = img
 		elif img.mode == "CMYK":
 			if args.mode == "gray":
-
+				target = img.convert("L")
 			elif args.mode == "cmyk":
-
+				target = img
 			elif args.mode == "rgb":
+				target = img.
+
 
 		elif img.mode == "RGB" or img.mode == "RGBA":
 			if img.mode == "RGBA":
 				img = img.convert("RGB")
 			if args.mode == "gray":
-
+				target = img.convert("L")
 			elif args.mode == "cmyk":
 
 			elif args.mode == "rgb":
+				target = img
 		else:
 			raise ()
 
@@ -103,20 +116,22 @@ for i, f in enumerate(args.images, 1):
 		else:
 			pass
 
-		if tg.mode == "L":
 
-		elif tg.mode == "CMYK":
-
-		elif tg.mode == "RGB":
-
-
+		# ハーフトーン化
+		if target.mode == "L":
+			halftone = halftone_grayscale_image(target, args.pitch)
+		elif target.mode == "CMYK":
+			halftone = halftone_cmyk_image(target)
+		elif target.mode == "RGB":
+			halftone = halftone_rgb_image(target)
+		# 目的の出力モードへ変換する
 		if halftone.mode == "L":
 
 		elif halftone.mode == "CMYK":
 
 		elif halftone.mode == "RGB":
 
-
+		# ファイルへ保存する
 		if complete.mode == "L":
 
 		elif complete.mode == "CMYK":
