aba9b308828eed29e7b8793cdbd68a5c5ff97b10
curegit <37978051+curegit@users.noreply.github.com>
2021-02-14 11:12:41 +0900

    Fix directory creation behaviour

--- modules/util.py
+++ modules/util.py
@@ -9,7 +9,7 @@ def eprint(*args, **kwargs):
 
 # ディレクトリが無ければ再帰的に作成する
 def mkdirp(path):
-	os.makedirs(path, exist_ok=True)
+	os.makedirs(os.path.normpath(path), exist_ok=True)
 
 # ディレクトリ、ファイル名、拡張子からパスを構築する
 def filepath(dirpath, filename, ext):
